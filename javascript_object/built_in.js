bbb0350f5a84b8f7c20bf6167d86e9c3a919d19a
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-09 15:36:01 +0900

    4.1. 객체는 언제 쓰나요?

---
+++ javascript_object/built_in.js
@@ -0,0 +1,5 @@
+//math라는 객체에  PI,random,floor를 정리가 되어 잇음
+console.log("Math.PI 의 값은", Math.PI); //3.14~~~ 
+console.log("Math.random() 의 값은",Math.random()); //랜덤
+console.log("Math.floor(3.9) 의 값은",Math.floor(3.9)) //소수점을 정수로
+
