eb5e86d54ef7ab416fca99932b000c359192debb
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-10 10:32:50 +0900

    7.2. prototype을 이용해서 재사용성을 높이기

--- javascript_object/prototype.js
+++ javascript_object/prototype.js
@@ -2,17 +2,20 @@ function person(name, first, second, third){
     this.name=name,
     this.first=first,
     this.second=second,
-    this.third=third,
-    this.sum=function(){
-        return this.first+this.second+this.third;
-    }
+    this.third=third
+}
+
+// person 이라는 생성자 함수 안에 sum이 없기때문에 한번만 정의되고 메모리 절약이 된다
+person.prototype.sum=function(){
+    return ':::::' +(this.first+this.second+this.third);
 }
 
 var kim = new person('kim',10,20,30);
-kim.sum = function(){
-    return '변경 :' + (this.first+this.second+this.third);
+kim.sum= function(){
+    return 'this :' +(this.first+this.second+this.third);
 }
 var lee = new person('lee',10,10,10);
-
 console.log("kim.sum() 의 값은",kim.sum()); // 60
 console.log("lee.sum() 의 값은",lee.sum()); // 30
+
+
