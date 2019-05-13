9295761107f40ec9155a8b5a280d2033de6fbe6c
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-13 14:51:45 +0900

    8.2. Classes 의 생성

---
+++ javascript_object/class.js
@@ -0,0 +1,15 @@
+class person{
+
+}
+
+var kim = new person();
+console.log('kim의 값은',kim);
+
+
+// var kim = new person('kim',10,20,30);
+// kim.sum= function(){
+//     return 'this :' +(this.first+this.second+this.third);
+// }
+// var lee = new person('lee',10,10,10);
+// console.log("kim.sum() 의 값은",kim.sum()); // 60
+// console.log("lee.sum() 의 값은",lee.sum()); // 30
