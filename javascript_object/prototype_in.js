f5f003870fdf691ba7fa8389c7689ccccbe44302
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-14 16:36:26 +0900

    13.4. 객체상속의 사용

--- javascript_object/prototype_in.js
+++ javascript_object/prototype_in.js
@@ -6,5 +6,27 @@ console.log('subObj.superVal 의 값은',subObj.superVal);
 subObj.superVal = 'sub';
 console.log('superObj.superVal 의 값은', superObj.superVal);
 
+var kim = {
+    name:'kim',
+    first:10, second:20,
+    sum:function(){return this.first+this.second}
+}
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 20;
+lee.avg = function(){
+    return(this.first+this.second)/2;
+}
 
-// __proto__ 를 대체 create() 에 대해서 설명함
+// var lee = {
+//     name:'lee',
+//     first:10, second:10,
+//     avg:function(){
+//         return (this.first+this.second)/2;
+//     }
+// }
+// lee.__proto__ = kim;
+
+console.log('kim.sum() 의 값은'+ kim.sum());
+console.log('lee.sum() 의 값은'+ lee.avg());
