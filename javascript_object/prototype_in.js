fe23358d8d84d90a06042c3b9c04b22eaf888ca9
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-14 16:28:26 +0900

    13.3. Object.create()

--- javascript_object/prototype_in.js
+++ javascript_object/prototype_in.js
@@ -1,10 +1,13 @@
 var superObj = {superVal:'super'}
-var subObj = {subVal:'sub'}
-subObj.__proto__ = superObj;
+// var subObj = {subVal:'sub'}
+
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub'
 console.log('subObj.subVal 의 값은',subObj.subVal);
 console.log('subObj.superVal 의 값은',subObj.superVal);
 subObj.superVal = 'sub';
 console.log('superObj.superVal 의 값은', superObj.superVal);
 
 
-// __proto__ 를 사용하면 객체를 다른객체의 자식으로 만들어버릴수 있다.
+// __proto__ 를 대체 create() 에 대해서 설명함
