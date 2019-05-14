fbf78eab7f6dff764aacb95cf815f57fdda2e853
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-14 15:43:57 +0900

    11. 상속

--- javascript_object/class.js
+++ javascript_object/class.js
@@ -5,17 +5,21 @@ class person{
         this.second = second;
     }
     sum(){
-        return ':::::' +(this.first+this.second);
+        return (this.first+this.second);
+    }
+
+}
+
+class personPlus extends person{
+    avg(){
+        return (this.first+this.second)/2;
     }
 }
 
-var kim = new person('kim',10,20);
-console.log('kim의 값은',kim);
+var kim = new personPlus('kim',10,20);
+console.log('kim.sum() 값은',kim.sum());
+console.log('kim.avg() 값은',kim.avg());
 
-var kim = new person('kim',10,20,30);
-kim.sum= function(){
-    return 'this :' +(this.first+this.second);
-}
-var lee = new person('lee',10,10,10);
-console.log("kim.sum() 의 값은",kim.sum()); 
-console.log("lee.sum() 의 값은",lee.sum()); 
+// 상속에 대해서 알아보자
+// 여러곳에서 쓸수도 있으니 코드를 수정하지않고 class를 하나더 만들면 된다.
+// 중복되는 코드가 있을경우 extends 를 사용하면 된다. 
