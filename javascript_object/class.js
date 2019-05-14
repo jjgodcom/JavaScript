8fd5bea440a8731df719d82ee277f188d9e6e9ce
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-14 16:07:59 +0900

    12. super

--- javascript_object/class.js
+++ javascript_object/class.js
@@ -25,7 +25,4 @@ var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
 console.log("kim.avg()", kim.avg());
 
-// super 라는 키워드에 대해서
-// super는 부모클래스를 불러서 부모클래스한테 일을 시키고 부모가하지 못하는일을 내가 처리하는것
-// super 뒤에 ()가 붙을 경우에는 부모클래스의 생성자이다.
-// syper 뒤에 .가 있을 경우에는 부모클래스이다.
+
