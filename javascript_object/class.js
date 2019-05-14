2ea2a66216849add6bd3a9720e4c04cfa23b0b4c
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-14 14:51:14 +0900

    10. 메소드 구현

--- javascript_object/class.js
+++ javascript_object/class.js
@@ -4,16 +4,18 @@ class person{
         this.first = first;
         this.second = second;
     }
+    sum(){
+        return ':::::' +(this.first+this.second);
+    }
 }
 
-var kim = new person('kim',10,20,30);
+var kim = new person('kim',10,20);
 console.log('kim의 값은',kim);
 
-
-// var kim = new person('kim',10,20,30);
-// kim.sum= function(){
-//     return 'this :' +(this.first+this.second+this.third);
-// }
-// var lee = new person('lee',10,10,10);
-// console.log("kim.sum() 의 값은",kim.sum()); // 60
-// console.log("lee.sum() 의 값은",lee.sum()); // 30
+var kim = new person('kim',10,20,30);
+kim.sum= function(){
+    return 'this :' +(this.first+this.second);
+}
+var lee = new person('lee',10,10,10);
+console.log("kim.sum() 의 값은",kim.sum()); 
+console.log("lee.sum() 의 값은",lee.sum()); 
