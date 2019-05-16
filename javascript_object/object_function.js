794e5052ff391c78bda48762efc167449cc5c4e7
jjgodcom_company <jjgodcom1@gmail.com>
2019-05-16 16:16:05 +0900

    14.3. bind

--- javascript_object/object_function.js
+++ javascript_object/object_function.js
@@ -1,4 +1,3 @@
-// 
 var kim = {name:'kim',first:10,second:20}
 var lee = {name:'lee',first:10,second:10}
 function sum(prefix){
@@ -8,6 +7,9 @@ function sum(prefix){
 console.log("sum.call(kim) 의 값은",sum.call(kim, '=>'));
 console.log("sum.call(lee) 의 값은",sum.call(lee, ':-'));
 
-// call이라는 함수를 사용할때는 
-// 첫번째인자는 그 함수의 내부의 this값 
-// 두번째인자부터는 파라미터 값을 넣는다
+var kimSum = sum.bind(kim, '->');
+console.log("kimSum() 의 값은",kimSum());
+
+
+
+
